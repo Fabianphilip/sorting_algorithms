98cce4c907754e7a6970da1782963fcb6df56a66
Fabian <125877740+Fabianphilip@users.noreply.github.com>
2023-07-24 05:27:40 -0700

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,48 +1,25 @@
 #include "sort.h"
-
 /**
- * swap_bubble - function for sorting with swap method.
- *@a: element to be swapped.
- *@b: element to be swapped.
- * Return: nothing.
+ * bubble_sort - sort array lements from min to max value
+ * @array: array
+ * @size: array size
  */
-
-void swap_bubble(int *a, int *b)
-{
-	int temp;
-
-	temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-/**
- * bubble_sort - sorts an array using bubble sort algorithm.
- *@array: array to be sorted.
- *@size: size to be sorted.
- * Return: void.
- */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, sz = size;
-	bool booln = false;
 
-	if (array == NULL || size < 2)
+	size_t i, index, tmp = 0;
+
+	if (size < 2)
 		return;
-
-	while (booln == false)
-	{
-		booln = true;
-		for (i = 0; i < sz - 1; i++)
+	for (i = 0; i < size; i++)
+		for (index = 0; index < size; index++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[index] > array[index + 1] && array[index + 1])
 			{
-				swap_bubble(array + i, array + i + 1);
-				print_array(array, size);
-				booln = false;
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
 			}
 		}
-		sz--;
-	}
 }
